825660be6948b489f92a7ac9fa2faeef0a39f057
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-08-30 16:59:12 +0300

    refactor users list screen

--- lib/features/app/presentaion/views/users_list_screen.dart
+++ lib/features/app/presentaion/views/users_list_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:linkyou_task/features/app/presentaion/manager/user_cubit.dart';
-import 'package:linkyou_task/features/app/presentaion/manager/user_state.dart';
+import 'widgets/user_list_screen_body.dart';
 
 class UsersListScreen extends StatelessWidget {
   const UsersListScreen({super.key});
@@ -10,34 +8,7 @@ class UsersListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Users List')),
-      body: BlocBuilder<UserCubit, UserState>(
-        builder: (context, state) {
-          if (state is UserLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is UserLoaded) {
-            final users = state.users;
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return ListTile(
-                  title: Text(user.name),
-                  subtitle: Text(user.email),
-                );
-              },
-            );
-          } else if (state is UserError) {
-            return Center(
-              child: Text(
-                state.message,
-                style: const TextStyle(color: Colors.red),
-              ),
-            );
-          } else {
-            return const Center(child: Text('No users available.'));
-          }
-        },
-      ),
+      body: const UserListScreenBody(),
     );
   }
 }
---
+++ lib/features/app/presentaion/views/widgets/user_list_screen_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:linkyou_task/features/app/presentaion/manager/user_cubit.dart';
+import 'package:linkyou_task/features/app/presentaion/manager/user_state.dart';
+
+class UserListScreenBody extends StatelessWidget {
+  const UserListScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, state) {
+        if (state is UserLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is UserLoaded) {
+          final users = state.users;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              return ListTile(
+                title: Text(user.name),
+                subtitle: Text(user.email),
+              );
+            },
+          );
+        } else if (state is UserError) {
+          return Center(
+            child: Text(
+              state.message,
+              style: const TextStyle(color: Colors.red),
+            ),
+          );
+        } else {
+          return const Center(child: Text('No users available.'));
+        }
+      },
+    );
+  }
+}
